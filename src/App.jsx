710c65fbbac6d15ee5c0d1792ed7d3f7093c3f29
Ayman Sedqi <sedqiayman02@gmail.com>
2024-05-05 12:47:36 +0100

    Fix images Issues

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,13 @@ function App() {
       <main role='main' className='flex w-full h-screen bg-veryDarkBlueBG flex-col sm:justify-center items-center pt-10 sm:pt-0'>
           
           <div className=' shadow-lg w-[350px] bg-veryDarkBlueCard p-5 rounded-lg flex flex-col gap-4'>
-            <img src="/image-equilibrium.jpg" alt="eth image" className=' rounded-lg' />
-            <h1 className='text-xl text-colorWhite'>Equilibrium #3429</h1>
+          <div className='w-full h-[280px] ethImage rounded-lg'>
+            <div className='hidden w-full bg-colorCyan  rounded-lg hover:flex cursor-pointer justify-center items-center h-full '>
+              <img className="" src="/icon-view.svg" alt="Icon image" />
+            </div>
+          </div>
+
+            <h1 className='text-xl text-colorWhite cursor-pointer hover:text-colorCyan'>Equilibrium #3429</h1>
             <p className=' font-thin text-softBlue'>Our Equilibrium collection promotes balance and calm.</p>
             <div className='flex justify-between bdrcolor pb-4'>
               <div className=' flex gap-2 items-center '>
@@ -24,7 +29,7 @@ function App() {
 
             <div className='flex gap-2 items-center'>
               <img className='w-8 border-white-2' src="/image-avatar.png" alt="Avatar" />
-              <p className=' text-sm text-colorWhite font-light'><span className='text-sm text-softBlue'>Creation of</span> Jules Wyvern</p>
+              <p className='cursor-pointer hover:text-colorCyan text-sm text-colorWhite font-light'><span className='text-sm text-softBlue '>Creation of</span> Jules Wyvern</p>
             </div>
 
           </div>
